583413c57183e012c8a33d5ccc3f42501fbcf4a6
Gaticus Hax <gaticus.hax@gmail.com>
2017-09-08 15:30:43 -0700

    - modified the behaviour of ShowVersion() so that the '--version' switch shows human-friendly version info (verbose) and the '-v' switch displays a program-friendly version string (compact)

--- MBINCompiler/Program.cs
+++ MBINCompiler/Program.cs
@@ -165,10 +165,11 @@ namespace MBINCompiler {
         /// Show the version string.
         /// </summary>
         /// <returns>Always returns 0 (exit code = success)</returns>
-        static int ShowVersion() {
+        static int ShowVersion(bool verbose = true) {
             string ver = GetVersionString();
-            // TODO: (GH) show full version string?
-            Console.WriteLine( ver.Substring( 0, ver.Length - 2 ) );
+            ver = ver.Substring( 0, ver.Length - 2 );
+            if (verbose) ver = $"libMBIN version {ver}";
+            Console.WriteLine( ver );
             return 0;
         }
 
@@ -235,8 +236,8 @@ namespace MBINCompiler {
             if (args[0] == "--help") return ShowHelp();
             if (args[0] == "-h")     return ShowHelp();
 
-            if (args[0] == "--version") return ShowVersion();
-            if (args[0] == "-v")        return ShowVersion();
+            if (args[0] == "--version") return ShowVersion(true);
+            if (args[0] == "-v")        return ShowVersion(false);
 
             // at least the first argument is an input param
 
