8f0b28ba0e095ca3049a1a2b7150f3f2eec47976
Gaticus Hax <gaticus.hax@gmail.com>
2018-10-18 18:19:51 -0700

    some fixes for potential errors in FileIO.LoadFile() and LoadMbin()

--- libMBIN/Source/Common/FileIO.cs
+++ libMBIN/Source/Common/FileIO.cs
@@ -1,43 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.IO;
 
-using libMBIN;
+namespace libMBIN {
 
-namespace libMBIN
-{
     /// <summary>
     /// Container class for general file loading methods.
     /// </summary>
-    public class FileIO
-    {
+    public class FileIO {
+
         /// <summary>
         /// Loads a file and returns an NMSTemplate which can be either used as-is, or cast to a specific type.
         /// The path to either an .exml or .mbin can be provided here and the correct method will be selected automatically.
         /// </summary>
         /// <param name="file">File path to the .exml or .mbin to be loaded into memory.</param>
         /// <returns>NMSTemplate</returns>
-        public static NMSTemplate LoadFile(string file)
-        {
-            NMSTemplate data = null;
-            if (Path.HasExtension(file))
-            {
-                string x = Path.GetExtension(file).ToUpper();
-                if (x == ".EXML")
-                {
-                    data = LoadExml(file);
-                }
-                else if (x == ".MBIN")
-                {
-                    data = LoadMbin(file);
-                }
-                else
-                {
-                    throw new InvalidDataException($"{file} does not have a supported file type. File type must be one of .exml or .mbin");
+        public static NMSTemplate LoadFile( string file ) {
+            // TODO: doesn't handle MBIN.PC files
+            if ( Path.HasExtension( file ) ) {
+                string x = Path.GetExtension( file ).ToUpper();
+                if ( x == ".EXML" ) {
+                    return LoadExml( file );
+                } else if ( x == ".MBIN" ) {
+                    return LoadMbin( file );
                 }
             }
-            return data;
+            throw new libMBIN.APIException( new InvalidDataException( $"{file} does not have a supported file type. File type must be one of .exml or .mbin" ) );
         }
 
         /// <summary>
@@ -45,23 +32,21 @@ namespace libMBIN
         /// </summary>
         /// <param name="path">File path to the .mbin to be loaded into memory.</param>
         /// <returns>NMSTemplate</returns>
-        public static NMSTemplate LoadMbin(string path)
-        {
-            MBINFile mbin = new MBINFile(path);
-            if (!mbin.LoadHeader() || !mbin.header.IsValid) throw new InvalidDataException("Not a valid MBIN file!");
+        public static NMSTemplate LoadMbin( string path ) {
+            string err = null;
+            try {
 
-            NMSTemplate data = null;
-            try
-            {
-                data = mbin.LoadData();
-                if (data is null) throw new InvalidDataException("Invalid MBIN data.");
-            }
-            catch (Exception e)
-            {
-                throw new MbinException($"Failed to read {mbin.header.GetXMLTemplateName()} from MBIN.", path, e);
-            }
+                MBINFile mbin = new MBINFile( path );
+                if ( !mbin.LoadHeader() || !mbin.header.IsValid ) throw new InvalidDataException( "Not a valid MBIN file!" );
 
-            return data;
+                NMSTemplate data = mbin.LoadData();
+                err = $"Failed to read {mbin.header.GetXMLTemplateName()} from MBIN";
+                if ( data is null ) throw new InvalidDataException( "Invalid MBIN data." );
+                return data;
+
+            } catch ( Exception e ) {
+                throw new MbinException( err, path, e );
+            }
         }
 
         /// <summary>
@@ -69,9 +54,10 @@ namespace libMBIN
         /// </summary>
         /// <param name="path">File path to the .exml to be loaded into memory.</param>
         /// <returns>NMSTemplate</returns>
-        public static NMSTemplate LoadExml(string path)
-        {
-            return EXmlFile.ReadTemplate(path);
+        public static NMSTemplate LoadExml( string path ) {
+            return EXmlFile.ReadTemplate( path );
         }
+
     }
+
 }
